2a242b9b335fc227f39553182d1d87a674fa855c
Raghav Verma <126396845+KateIronic@users.noreply.github.com>
2024-12-06 09:28:43 +0000

    init

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
+
+@Component({
+  selector: 'app-user',
+  imports: [SignupComponent],
+  templateUrl: './user.component.html',
+  styles: ``
+})
+export class UserComponent {
+
+}
