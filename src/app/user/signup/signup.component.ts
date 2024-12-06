cc525316f6fe8bb5f5c5be838edcc6df6dae117a
Raghav Verma <126396845+KateIronic@users.noreply.github.com>
2024-12-06 14:33:53 +0000

    modified

--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormControl, ReactiveFormsModule, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styles: ``
+  styles: ``,
+  standalone:true
 })
 export class SignupComponent {
 
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
+
