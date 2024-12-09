56cd67b4fd6c0f3da1de7369b68e25b15e14968c
Raghav Verma <vermaraghav265@gmail.com>
2024-12-09 23:09:23 +0530

    modified

--- src/app/components/start-learning/start-learning.component.ts
+++ src/app/components/start-learning/start-learning.component.ts
@@ -8,13 +8,5 @@ import { AdCardComponent } from '../ad-card/ad-card.component';
   styles: ``
 })
 export class StartLearningComponent {
-<<<<<<< HEAD
-=======
-  // adCardData = {
-  //   name: 'Amazing Adventure',
-  //   durations: '5 Days & 4 Nights',
-  //   image: 'https://static.uacdn.net/web-cms/highlightgoals_96d24a04e7_a8e8de225d.svg?q=75&auto=format%2Ccompress&w=640'
-  // };
->>>>>>> 891f3b3f22b06876b3d4c2b92d393eec2268999b
 
 }
