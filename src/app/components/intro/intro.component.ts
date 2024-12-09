891f3b3f22b06876b3d4c2b92d393eec2268999b
Raghav Verma <126396845+KateIronic@users.noreply.github.com>
2024-12-09 10:40:29 +0000

    start learning added

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { UserComponent } from "../../user/user.component";
+import { GoalComponent } from '../goal/goal.component';
+import { StartLearningComponent } from '../start-learning/start-learning.component';
 
 @Component({
   selector: 'app-intro',
-  imports: [NavComponent, UserComponent],
+  imports: [NavComponent, UserComponent, GoalComponent, StartLearningComponent],
   templateUrl: './intro.component.html',
   styles: ``
 })
---
+++ src/app/components/start-learning/start-learning.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AdCardComponent } from '../ad-card/ad-card.component';
+
+@Component({
+  selector: 'app-start-learning',
+  imports: [AdCardComponent],
+  templateUrl: './start-learning.component.html',
+  styles: ``
+})
+export class StartLearningComponent {
+  // adCardData = {
+  //   name: 'Amazing Adventure',
+  //   durations: '5 Days & 4 Nights',
+  //   image: 'https://static.uacdn.net/web-cms/highlightgoals_96d24a04e7_a8e8de225d.svg?q=75&auto=format%2Ccompress&w=640'
+  // };
+
+}
